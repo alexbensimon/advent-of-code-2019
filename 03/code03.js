b87da5e4d0f917662510f83c69324fbf12ee6b3a
Alexandre Bensimon <alexandrebensimon1@gmail.com>
2019-12-03 20:29:30 +0100

    wip

--- 03/code03.js
+++ 03/code03.js
@@ -6,7 +6,7 @@ const [path1, path2] = buffer
   .split('\n')
   .map(path => path.split(','));
 
-const executeInstructions = (pos, instructions, index, trace) => {
+const executeInstructions = (pos, instructions, index, matrix, pathNumber) => {
   const instruction = instructions[index];
   const direction = instruction.slice(0, 1);
   const length = Number(instruction.slice(1));
@@ -15,44 +15,64 @@ const executeInstructions = (pos, instructions, index, trace) => {
     L: goLeft,
     U: goUp,
     D: goDown,
-  }[direction](pos, length, trace);
+  }[direction](pos, length, matrix, pathNumber);
   if (index + 1 < instructions.length) {
-    executeInstructions(newPos, instructions, index + 1, trace);
+    executeInstructions(newPos, instructions, index + 1, matrix, pathNumber);
   } else {
     return;
   }
 };
 
-const goRight = (pos, length, trace) => {
+const goRight = (pos, length, matrix, pathNumber) => {
   for (let i = 1; i <= length; i++) {
-    trace.push([pos[0] + i, pos[1]]);
+    if (matrix[pos[0] + i] !== undefined) {
+      matrix[pos[0] + i][pos[1]] = {
+        ...matrix[pos[0] + i][pos[1]],
+        [pathNumber]: true,
+      };
+    }
   }
   return { newPos: [pos[0] + length, pos[1]] };
 };
 
-const goLeft = (pos, length, trace) => {
+const goLeft = (pos, length, trace, pathNumber) => {
   for (let i = 1; i <= length; i++) {
-    trace.push([pos[0] - i, pos[1]]);
+    if (matrix[pos[0] - i] !== undefined) {
+      matrix[pos[0] - i][pos[1]] = {
+        ...matrix[pos[0] - i][pos[1]],
+        [pathNumber]: true,
+      };
+    }
   }
   return { newPos: [pos[0] - length, pos[1]] };
 };
 
-const goUp = (pos, length, trace) => {
+const goUp = (pos, length, trace, pathNumber) => {
   for (let i = 1; i <= length; i++) {
-    trace.push([pos[0], pos[1] + i]);
+    if (matrix[pos[0]] !== undefined) {
+      matrix[pos[0]][pos[1] + i] = {
+        ...matrix[pos[0]][pos[1] + i],
+        [pathNumber]: true,
+      };
+    }
   }
   return { newPos: [pos[0], pos[1] + length] };
 };
 
-const goDown = (pos, length, trace) => {
+const goDown = (pos, length, trace, pathNumber) => {
   for (let i = 1; i <= length; i++) {
-    trace.push([pos[0], pos[1] - i]);
+    if (matrix[pos[0]] !== undefined) {
+      matrix[pos[0]][pos[1] - i] = {
+        ...matrix[pos[0]][pos[1] - i],
+        [pathNumber]: true,
+      };
+    }
   }
   return { newPos: [pos[0], pos[1] - length] };
 };
 
-const testPath1 = ['R8', 'U5', 'L5', 'D3'];
-const testPath2 = ['U7', 'R6', 'D4', 'L4'];
+// const testPath1 = ['R8', 'U5', 'L5', 'D3'];
+// const testPath2 = ['U7', 'R6', 'D4', 'L4'];
 
 // const testPath1 = [
 //   'R75',
@@ -93,10 +113,23 @@ const testPath2 = ['U7', 'R6', 'D4', 'L4'];
 //   'R7',
 // ];
 
-const trace1 = [];
-executeInstructions([0, 0], path1, 0, trace1);
-const trace2 = [];
-executeInstructions([0, 0], path2, 0, trace2);
+const size = 6000;
+const matrix = new Array(size).fill({}).map(() => new Array(size).fill({}));
+executeInstructions([size / 2, size / 2], path1, 0, matrix, 'a');
+executeInstructions([size / 2, size / 2], path2, 0, matrix, 'b');
+
+const distances = [];
+for (i = 0; i < size; i++) {
+  for (j = 0; j < size; j++) {
+    if (matrix[i][j]['a'] && matrix[i][j]['b']) {
+      distances.push(i - size / 2 + j - size / 2);
+      console.log(i, j);
+    }
+  }
+}
+
+console.log('TCL: distances', distances);
+console.log(Math.min(...distances.map(distance => Math.abs(distance))));
 
 // const sameCoords = [];
 // trace1.forEach(coords1 => {
@@ -113,37 +146,37 @@ executeInstructions([0, 0], path2, 0, trace2);
 //   return prev;
 // }, 10000000);
 
-let sameCoords = [];
+// let sameCoords = [];
 
-// while(i < 300) {
-//   while(j < 300) {}
+// for (let i = 0; i < 2000; i++) {
+//   for (let j = 0; j < 2000; j++) {
+//     console.log(i, j);
+//     let isSame1 = false;
+//     trace1.forEach(coords => {
+//       if (coords[0] === i && coords[1] === j) {
+//         console.log('TCL: trace1 coords', coords);
+//         isSame1 = true;
+//       }
+//     });
+//     if (isSame1) {
+//       let isSame2 = false;
+//       trace2.forEach(coords => {
+//         if (coords[0] === i && coords[1] === j) {
+//           console.log('TCL: trace2 coords', coords);
+//           isSame2 = true;
+//         }
+//       });
+
+//       if (isSame1 && isSame2) {
+//         sameCoords.push(i + j);
+//       }
+//     }
+//   }
 // }
 
-for (let i = -200; i < 0; i++) {
-  for (let j = -200; j < 0; j++) {
-    console.log(i, j);
-    let isSame1 = false;
-    trace1.forEach(coords => {
-      if (coords[0] === i && coords[1] === j) {
-        console.log('TCL: trace1 coords', coords);
-        isSame1 = true;
-      }
-    });
-    if (isSame1) {
-      let isSame2 = false;
-      trace2.forEach(coords => {
-        if (coords[0] === i && coords[1] === j) {
-          console.log('TCL: trace2 coords', coords);
-          isSame2 = true;
-        }
-      });
+// console.log('TCL: sameCoords', sameCoords);
+// console.log('MANHATTAN', Math.min(...sameCoords));
 
-      if (isSame1 && isSame2) {
-        sameCoords.push(Math.abs(i) + Math.abs(j));
-      }
-    }
-  }
-}
+// const lookForCoordsAtDistance = (distance) => {
 
-console.log('TCL: sameCoords', sameCoords);
-console.log('MANHATTAN', Math.min(...sameCoords));
+// }
